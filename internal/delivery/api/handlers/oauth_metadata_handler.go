e2d9fd3a27e8b103de06571cb61dc31653f3c2b7
Jeff Wu <itmrchow@gmail.com>
2025-07-23 13:36:02 +0800

    feat: Update OAuth metadata handler to use dynamic APP_URL and PORT for authorization servers

--- internal/delivery/api/handlers/oauth_metadata_handler.go
+++ internal/delivery/api/handlers/oauth_metadata_handler.go
@@ -1,11 +1,13 @@
 package handlers
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
 	"github.com/mark3labs/mcp-go/client/transport"
 	"github.com/rs/zerolog"
+	"github.com/spf13/viper"
 
 	"course-mcp/internal/usecase/utils"
 )
@@ -23,8 +25,11 @@ func NewOAuthMetadataHandler(logger *zerolog.Logger, authProvider utils.AuthProv
 }
 
 func (h *OAuthMetadataHandler) HandleOAuthProtectedResource(c *gin.Context) {
+	authorizationServers := fmt.Sprintf("%s:%s/realms/course_server/.well-known/openid-configuration", viper.GetString("APP_URL"), viper.GetString("PORT"))
+
 	metadata := &transport.OAuthProtectedResource{
-		AuthorizationServers: []string{"http://localhost:3000/realms/course_server/.well-known/openid-configuration"},
+		// TODO: url
+		AuthorizationServers: []string{authorizationServers},
 		Resource:             "Example OAuth Protected Resource",
 		ResourceName:         "Example OAuth Protected Resource",
 	}
@@ -43,6 +48,7 @@ func (h *OAuthMetadataHandler) HandleOAuthAuthorizationServer(c *gin.Context) {
 	}
 
 	// change endpoint URLs to localhost:3000
+	// TODO: url
 	metadata.AuthorizationEndpoint = "http://localhost:3000/authorize"
 	metadata.TokenEndpoint = "http://localhost:3000/token"
 	metadata.RegistrationEndpoint = "http://localhost:3000/register"
